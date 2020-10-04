f726bc8832758c20429e72afe2f63640acf35827
Florian Weber <fl-weber@t-online.de>
2020-10-04 12:44:25 +0200

    feat: added eslint configs

---
+++ packages/eslint-config-typescript/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@averjs',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+}
